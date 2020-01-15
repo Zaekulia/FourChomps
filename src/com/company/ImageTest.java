0e702e91764f31c8aa5962f3c5d6bb32d3ff60ef
smuryson <marie_kairies@gmx.de>
2020-01-15 15:30:17 +0100

    big frying pan

---
+++ src/com/company/ImageTest.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImageTest {
+    private JPanel rootPanel;
+    private JLabel JImage;
+    ImageTest(){
+        Image img =new ImageIcon(this.getClass().getResource("/ca.png")).getImage();
+        JImage.setIcon(new ImageIcon(img));
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ImageTest");
+        frame.setContentPane(new ImageTest().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+}
