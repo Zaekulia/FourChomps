0ae3ccb24323fe8421d2bf277ae8abba524e2d5e
smuryson <marie_kairies@gmx.de>
2020-01-18 11:07:42 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Four_Chomps/com/company/Anmeldung.class
    #	out/production/Four_Chomps/com/company/Client$1.class
    #	out/production/Four_Chomps/com/company/Client$2.class
    #	out/production/Four_Chomps/com/company/ServerConnection.class
    #	out/production/Four_Chomps/com/company/ServerStatus.class

---
+++ src/com/company/WinForm.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+
+public class WinForm {
+    private JPanel rootPanel;
+    private JPanel WinPanel;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a7Button;
+}
